7fde0e7a72995c8cb1e4a51c2c327c19349e1911
Alex <alex95.taylor@gmail.com>
2024-05-12 21:03:10 -0400

    Implement initial game logic
    -Load word in and display it as dashes
    -When letter is clicked it is checked for a match
     within the word. If there is a match then replace
     the dash with the correct letter.

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -1,17 +1,90 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import HangmanService from "../HangmanService";
 
 function Game() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [clickedLetters, setClickedLetters] = useState([]);
+  const [word, setWord] = useState();
+  const [dashes, setDashes] = useState([]);
 
   const topic = searchParams.get("topic");
   const category = searchParams.get("category");
+
+  useEffect(() => {
+    const fetchWord = async () => {
+      try {
+        const wordData = await HangmanService.getWordFromCategory(category);
+        //Turn word into array of letters
+        setWord(wordData.toUpperCase().split(""));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchWord();
+  }, []);
+
+  //Create dash array after word has been loaded
+  useEffect(() => {
+    let dashArr = [];
+    //Create array with dashes for each letter
+    if (word) {
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === " ") {
+          dashArr.push(" ");
+        } else {
+          dashArr.push("-");
+        }
+      }
+    }
+    setDashes(dashArr);
+  }, [word]);
+
+  //Check for game win/lose conditions
+  useEffect(() => {
+    if (word) {
+      if (checkWin(dashes, word)) {
+        console.log("WIN");
+      }
+    }
+  }, [dashes, word]);
+
+  const displayWordAsDashes = () => {
+    return (
+      <div className="flex justify-center gap-3 border p-2 md:w-3/4  lg:w-1/3">
+        {dashes.map((dash, index) => (
+          <p className="text-3xl font-bold" key={index}>
+            {dash}
+          </p>
+        ))}
+      </div>
+    );
+  };
+
+  const checkLetter = (letter) => {
+    let updatedDashes = [...dashes];
+    for (let i = 0; i < word.length; i++) {
+      if (letter === word[i]) {
+        updatedDashes[i] = word[i];
+      }
+    }
+    setDashes(updatedDashes);
+  };
+
+  const checkWin = (dashes, word) => {
+    if (dashes.join("") === word.join("")) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   const handleClick = (letter) => {
     //Add letter to clickedLetters array if it isn't there
     if (!clickedLetters.includes(letter)) {
       setClickedLetters([...clickedLetters, letter]);
+      checkLetter(letter);
     }
   };
 
@@ -21,7 +94,7 @@ function Game() {
       .split("");
 
     return (
-      <div className="grid grid-cols-7 justify-items-center gap-2 p-2 md:mx-auto md:w-3/4 md:grid-cols-10 md:gap-4 lg:w-1/3">
+      <div className="grid grid-cols-7 justify-items-center gap-2 p-2 md:mx-auto md:grid-cols-10 md:gap-4 lg:w-1/3">
         {alphabet.map((letter, index) => (
           <div
             key={index}
@@ -39,10 +112,15 @@ function Game() {
     );
   };
 
+  if (!word) {
+    return <p>Loading . . . </p>;
+  }
+
   return (
     <div className="flex flex-col items-center">
       <h1>Topic: {topic}</h1>
       <h1>Category: {category}</h1>
+      {word && displayWordAsDashes()}
       <div className="sm:w-3/4">{displayKeyboard()}</div>
     </div>
   );
