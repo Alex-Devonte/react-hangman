a2c638a9eccd5def74945d0a22463a4633f4fdcf
Alex <alex95.taylor@gmail.com>
2024-10-16 22:28:26 -0400

    Update urls

--- frontend/src/HangmanService.js
+++ frontend/src/HangmanService.js
@@ -1,12 +1,13 @@
 import axios from "axios";
+const baseURL = "https://react-hangman-3uqj.onrender.com";
 
 const getCategories = async () => {
-  const response = await axios.get("/hangman");
+  const response = await axios.get(`${baseURL}/hangman`);
   return response.data;
 };
 
 const getWordFromCategory = async (category) => {
-  const response = await axios.get("/hangman/words", {
+  const response = await axios.get(`${baseURL}hangman/words`, {
     params: { category: category },
   });
   return response.data;
