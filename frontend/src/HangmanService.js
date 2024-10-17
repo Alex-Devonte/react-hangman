b7a20bedcd72c14636d6a8e858dbc1a727607a38
Alex <alex95.taylor@gmail.com>
2024-10-17 13:24:19 -0400

    Implement env variable for url

--- frontend/src/HangmanService.js
+++ frontend/src/HangmanService.js
@@ -1,15 +1,17 @@
 import axios from "axios";
-const baseURL = "https://react-hangman-3uqj.onrender.com";
 
 const getCategories = async () => {
-  const response = await axios.get(`${baseURL}/hangman`);
+  const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/hangman`);
   return response.data;
 };
 
 const getWordFromCategory = async (category) => {
-  const response = await axios.get(`${baseURL}hangman/words`, {
-    params: { category: category },
-  });
+  const response = await axios.get(
+    `${import.meta.env.VITE_BASE_URL}/hangman/words`,
+    {
+      params: { category: category },
+    },
+  );
   return response.data;
 };
 
