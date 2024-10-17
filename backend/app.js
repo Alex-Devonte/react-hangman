d52d64555ab88f8c1c2c7c737dc54ce17e5d13ee
Alex <alex95.taylor@gmail.com>
2024-10-16 22:18:51 -0400

    Remove serve frontend logic

--- backend/app.js
+++ backend/app.js
@@ -28,13 +28,6 @@ app.get("/hangman/words", async (req, res) => {
   res.send(data);
 });
 
-//Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html")),
-  );
-}
 
 // error handler
 app.use(function (err, req, res, next) {
